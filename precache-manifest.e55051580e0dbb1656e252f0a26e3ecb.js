9d59a9d6bea483d7f93399b6a59f20220920c455
mayank-genesis <mayank16meena@gmail.com>
2020-05-02 01:27:45 +0530

    Updates

---
+++ precache-manifest.e55051580e0dbb1656e252f0a26e3ecb.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175a408839b8e33dc5d5f3734dad530c",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "f715b1031d014f601124",
+    "url": "/MyPortfolio/static/css/main.d7091a5d.chunk.css"
+  },
+  {
+    "revision": "2149366414bccbc2ae6d",
+    "url": "/MyPortfolio/static/js/2.add0b28c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyPortfolio/static/js/2.add0b28c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f715b1031d014f601124",
+    "url": "/MyPortfolio/static/js/main.b74b2119.chunk.js"
+  },
+  {
+    "revision": "5b821521451268ee28ae",
+    "url": "/MyPortfolio/static/js/runtime-main.3e2257a3.js"
+  },
+  {
+    "revision": "6051808785cc17751ef4175b3a84e31d",
+    "url": "/MyPortfolio/static/media/MyFirstWebsite.60518087.png"
+  },
+  {
+    "revision": "8b16c915cc11116c8b669b3a289213fd",
+    "url": "/MyPortfolio/static/media/PathFindingVisualizer.8b16c915.png"
+  },
+  {
+    "revision": "e2e7c219f2ebb7a1c2c6e04b4938cc9c",
+    "url": "/MyPortfolio/static/media/Resume-Mayank-Meena.e2e7c219.pdf"
+  },
+  {
+    "revision": "704f8c35a81ddac0e185fb6ba5968b47",
+    "url": "/MyPortfolio/static/media/SnakeQuarantined.704f8c35.png"
+  },
+  {
+    "revision": "1b838c6c2873b82f25fe0d91ee5a24ff",
+    "url": "/MyPortfolio/static/media/Untitled.1b838c6c.png"
+  },
+  {
+    "revision": "7d258953df29ef9bb0d53f4207da22b2",
+    "url": "/MyPortfolio/static/media/arduino.7d258953.jpg"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/MyPortfolio/static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "097b77494452f9352630bcf79697db4d",
+    "url": "/MyPortfolio/static/media/insta.097b7749.png"
+  },
+  {
+    "revision": "a30d60b6a080c925f08d8204ccc6ab4d",
+    "url": "/MyPortfolio/static/media/instructables-logo.a30d60b6.png"
+  },
+  {
+    "revision": "828e4779415a6ad47ff7fa9d3c4a5772",
+    "url": "/MyPortfolio/static/media/mail.828e4779.png"
+  },
+  {
+    "revision": "e9208d30bade0ca8ae81145c9e7c8e8c",
+    "url": "/MyPortfolio/static/media/mayank.e9208d30.jpg"
+  },
+  {
+    "revision": "7541b24c78a983724a2385fbc35e4bc2",
+    "url": "/MyPortfolio/static/media/twitter.7541b24c.png"
+  },
+  {
+    "revision": "c590943d4368f53e9884a61dd5144b21",
+    "url": "/MyPortfolio/static/media/welcome.c590943d.php"
+  },
+  {
+    "revision": "b01ed54a77952a0438e09b5328a528b8",
+    "url": "/MyPortfolio/static/media/youtube.b01ed54a.png"
+  }
+]);
---
+++ static/media/welcome.c590943d.php
@@ -0,0 +1,3 @@
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
